f3994d3fba00ef6b38b8eed3ef433361f6564c07
Danfer Habed <dhabed@outlook.com>
2022-08-17 10:14:03 -0600

    Add project files.

---
+++ NavigationBehavior.cs
@@ -0,0 +1,28 @@
+﻿using DevExpress.Mvvm.UI.Interactivity;
+using DevExpress.Xpf.Editors;
+using DevExpress.Xpf.Grid;
+
+namespace CellTemplate {
+    public class NavigationBehavior : Behavior<TableView> {
+        protected override void OnAttached() {
+            base.OnAttached();
+            AssociatedObject.ProcessEditorActivationAction += TableView_ProcessEditorActivationAction;
+            AssociatedObject.GetActiveEditorNeedsKey += TableView_GetActiveEditorNeedsKey;
+        }
+        protected override void OnDetaching() {
+            AssociatedObject.ProcessEditorActivationAction -= TableView_ProcessEditorActivationAction;
+            AssociatedObject.GetActiveEditorNeedsKey -= TableView_GetActiveEditorNeedsKey;
+            base.OnDetaching();
+        }
+        void TableView_ProcessEditorActivationAction(object sender, ProcessEditorActivationActionEventArgs e) {
+            if (e.Column.FieldName == nameof(Place.City))
+                if (e.ActivationAction == ActivationAction.MouseLeftButtonDown && e.MouseLeftButtonEventArgs.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+                    e.RaiseEventAgain = true;
+        }
+        void TableView_GetActiveEditorNeedsKey(object sender, GetActiveEditorNeedsKeyEventArgs e) {
+            if (e.Column.FieldName == nameof(Place.City))
+                if (e.Key == System.Windows.Input.Key.Up || e.Key == System.Windows.Input.Key.Down)
+                    e.NeedsKey = true;
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.Mvvm;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CellTemplate {
+    public class ViewModel : ViewModelBase {
+        public ViewModel() {
+            Source = Places.GetPlaces();
+        }
+        public ObservableCollection<Place> Source { get; set; }
+    }
+
+    public class Place {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public List<string> Cities { get; set; }
+    }
+    public static class Places {
+        public static ObservableCollection<Place> GetPlaces() {
+            ObservableCollection<Place> places = new ObservableCollection<Place>();
+            places.Add(new Place() { Country = "USA", City = "Washington, D.C.", Cities = new List<string> { "Washington, D.C.", "New York", "Los Angeles", "Las Vegas" } });
+            places.Add(new Place() { Country = "Germany", City = "Berlin", Cities = new List<string> { "Berlin", "Munich", "Frankfurt" } });
+            places.Add(new Place() { Country = "United Kingdom", City = "London", Cities = new List<string> { "London", "Birmingham" } });
+            places.Add(new Place() { Country = "Canada", City = "Montreal", Cities = new List<string> { "Montreal", "Toronto" } });
+            places.Add(new Place() { Country = "China", City = "Beijing", Cities = new List<string> { "Beijing", "Tianjin", "Shanghai", "Chongqing" } });
+            return places;
+        }
+    }
+}
