3d6f26d92dea8e5c2db748d3108ef00c7bc32e04
Danfer Habed <dhabed@outlook.com>
2022-08-17 10:54:38 -0600

    New column

--- ViewModel.cs
+++ ViewModel.cs
@@ -12,17 +12,18 @@ namespace CellTemplate {
 
     public class Place {
         public string Country { get; set; }
+        public string Comments { get; set; }
         public string City { get; set; }
         public List<string> Cities { get; set; }
     }
     public static class Places {
         public static ObservableCollection<Place> GetPlaces() {
             ObservableCollection<Place> places = new ObservableCollection<Place>();
-            places.Add(new Place() { Country = "USA", City = "Washington, D.C.", Cities = new List<string> { "Washington, D.C.", "New York", "Los Angeles", "Las Vegas" } });
-            places.Add(new Place() { Country = "Germany", City = "Berlin", Cities = new List<string> { "Berlin", "Munich", "Frankfurt" } });
-            places.Add(new Place() { Country = "United Kingdom", City = "London", Cities = new List<string> { "London", "Birmingham" } });
-            places.Add(new Place() { Country = "Canada", City = "Montreal", Cities = new List<string> { "Montreal", "Toronto" } });
-            places.Add(new Place() { Country = "China", City = "Beijing", Cities = new List<string> { "Beijing", "Tianjin", "Shanghai", "Chongqing" } });
+            places.Add(new Place() { Country = "USA", Comments = "Hello", City = "Washington, D.C.", Cities = new List<string> { "Washington, D.C.", "New York", "Los Angeles", "Las Vegas" } });
+            places.Add(new Place() { Country = "Germany", Comments = "Hello", City = "Berlin", Cities = new List<string> { "Berlin", "Munich", "Frankfurt" } });
+            places.Add(new Place() { Country = "United Kingdom", Comments = "Hello", City = "London", Cities = new List<string> { "London", "Birmingham" } });
+            places.Add(new Place() { Country = "Canada", Comments = "Hello", City = "Montreal", Cities = new List<string> { "Montreal", "Toronto" } });
+            places.Add(new Place() { Country = "China", Comments = "Hello", City = "Beijing", Cities = new List<string> { "Beijing", "Tianjin", "Shanghai", "Chongqing" } });
             return places;
         }
     }
